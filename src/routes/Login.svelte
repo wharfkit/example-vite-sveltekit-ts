ee1af02d4cbc8ac278443586c570533625127696
Aaron Cox <aaron@greymass.com>
2023-07-31 14:30:07 -0700

    Updating to 1.0.0

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-    import SessionKit, { BrowserLocalStorage, Session } from '@wharfkit/session'
+    import { Chains, Session, SessionKit } from '@wharfkit/session'
     import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
-    import { WalletPluginCloudWallet } from '@wharfkit/wallet-plugin-cloudwallet'
     import WebRenderer from '@wharfkit/web-renderer'
     import { browser } from '$app/environment'
     import { onMount } from 'svelte'
@@ -14,30 +13,9 @@
 
         sessionKit = new SessionKit({
             appName: 'demo',
-            chains: [
-                {
-                    id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-                    url: 'https://eos.greymass.com',
-                },
-                {
-                    id: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
-                    url: 'https://telos.greymass.com',
-                },
-                {
-                    id: '8fc6dce7942189f842170de953932b1f66693ad3788f766e777b6f9d22335c02',
-                    url: 'https://api.uxnetwork.io',
-                },
-                {
-                    id: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-                    url: 'https://wax.greymass.com',
-                },
-            ],
-            storage: new BrowserLocalStorage('demo'),
+            chains: [Chains.Jungle4],
             ui,
-            walletPlugins: [
-                new WalletPluginAnchor(),
-                new WalletPluginCloudWallet(),
-            ],
+            walletPlugins: [new WalletPluginAnchor()],
         })
 
         onMount(async () => {
